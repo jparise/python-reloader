6ccf42acb21b2d24852ab933f3cf6fe5cdf39202
Jon Parise <jon@indelible.org>
2010-01-08 09:45:56 -0800

    Adding license information (MIT License).

--- reloader.py
+++ reloader.py
@@ -1,5 +1,24 @@
 # Python Module Reloader
-# Jon Parise <jon@indelible.org>
+#
+# Copyright (c) 2009, 2010 Jon Parise <jon@indelible.org>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 
 import builtins
 import os
@@ -10,6 +29,7 @@ import time
 import threading
 from collections import OrderedDict
 
+__author__  = 'Jon Parise <jon@indelible.org>'
 __version__ = '0.1'
 
 _win = (sys.platform == 'win32')
