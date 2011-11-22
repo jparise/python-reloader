a93af75707ecadb4ab7d9ef0ea3bb1f550a5f902
Jon Parise <jon@indelible.org>
2011-11-22 13:43:10 -0800

    Update the package metadata for PyPI.
    This relates to the PyPI release requested in #6.

--- setup.py
+++ setup.py
@@ -5,15 +5,16 @@ from distutils.core import setup
 version = __import__('reloader').__version__
 
 setup(
-    name = 'Reloader',
+    name = 'reloader',
     version = version,
     description = 'Dependency-based Python module reloader',
     author = 'Jon Parise',
     author_email = 'jon@indelible.org',
     license = "MIT License",
-    url = "http://www.indelible.org/ink/python-reloading/",
+    url = "https://github.com/jparise/python-reloader",
     classifiers = ['Intended Audience :: Developers',
                    'License :: OSI Approved :: MIT License',
+                   'Development Status :: 4 - Beta',
                    'Operating System :: OS Independent',
                    'Programming Language :: Python'],
     py_modules = ['reloader'],
