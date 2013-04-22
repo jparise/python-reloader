ed3bdc03496b182b4e7143b4cd85daa4324cb2d5
Jon Parise <jon@indelible.org>
2013-04-21 22:32:52 -0700

    Extend the copyright year range to include 2014.

--- reloader.py
+++ reloader.py
@@ -1,6 +1,6 @@
 # Python Module Reloader
 #
-# Copyright (c) 2009, 2010, 2011, 2012 Jon Parise <jon@indelible.org>
+# Copyright (c) 2009-2014 Jon Parise <jon@indelible.org>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
